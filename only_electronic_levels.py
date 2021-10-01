0f81d3b208f6d6a7a45d78234ff177cbaaf1faf2
abejarano002 <abejaransanchez@gmail.com>
2021-10-01 12:48:25 +0200

    singular_matrix ¿?

--- only_electronic_levels.py
+++ only_electronic_levels.py
@@ -9,93 +9,94 @@ b=10
 def fermi_dirac(e,mu):
     return 1/(np.exp((e-mu)*b)+1)
  
+
 #(0,e,g,2)=(0,1,2,3)
 ed=0.
 delta=1.
 #here tunneling rates for \Gamma_{Le,lg,re,rg}
+#tunneling rate as semicircle with wide w 
+def semi_circle(e, mu, w):
+    e = np.array(e).reshape(-1, 1)
+    mu = np.array(mu).reshape(1, -1)
+    x = (e - mu) / w # broadcasting                                                                                                                                                                                
+    x = np.clip(x, -1, 1) # values outside the band width should be set to zero
+    y = (1 - x ** 2) ** 0.5
+    return np.squeeze(y)
+
 lg=1.
 le=1.
 rg=1.
 re=1.
+w=10
 
-#electron_holes = a = 0,1
-# 0 if electrons
-# 1 if holes
-def tunneling_rates(mul,mur,a):
-    rate = np.zeros((2,2))
-    rate[0,0] = lg * (a-((fermi_dirac(0, mul))*((-1)**(a+1))))
-    rate[0,1] = le * (a-((fermi_dirac(delta, mul))*((-1)**(a+1))))
-    rate[1,0] = rg * (a-((fermi_dirac(0, mur))*((-1)**(a+1))))
-    rate[1,1] = re * (a-((fermi_dirac(delta, mur))*((-1)**(a+1))))
-    return rate 
+VL = VR = np.linspace(-12, 12, 11)
+tu_Lg = lg * semi_circle(ed, VL,w)
+tu_Le = le * semi_circle(ed, VL,w)
+tu_Rg = rg * semi_circle(ed, VR,w)
+tu_Re = re * semi_circle(ed, VR,w)
 
-#print(tunneling_rates(1,1,1))  
-#here we define:
-#gamma_{l,r} matrix for electrons and holes: a
-def trans_rate_in(mul,mur):
-    gamma_le = np.zeros((4,4))
-    gamma_re = np.zeros((4,4))
-    gamma_lh = np.zeros((4,4))
-    gamma_rh = np.zeros((4,4))
-    for i in range(1,3):  
-             gamma_le[i,0] = tunneling_rates(mul,mur,0)[0,i-1]
-             gamma_le[3,i] = gamma_le[i,0]
-             gamma_re[i,0] = tunneling_rates(mul,mur,0)[1,i-1]
-             gamma_re[3,i] = gamma_le[i,0]
-             #holes
-             gamma_lh[0,i] = tunneling_rates(mul,mur,1)[0,i-1]
-             gamma_lh[i,3] = gamma_lh[0,i]
-             gamma_rh[0,i] = tunneling_rates(mul,mur,1)[1,i-1]
-             gamma_rh[i,3] = gamma_lh[0,i]
-    return gamma_le, gamma_re ,gamma_lh ,gamma_rh 
-
-#Here a transition rates matrix
-def trans_rate(mul,mur):
-    trans_rate = np.zeros((4,4)) 
-    #we start defining off diagonal elements
-    trans_rate = trans_rate_in(mul,mur)[0]+\
-         trans_rate_in(mul,mur)[1]+\
-           trans_rate_in(mul,mur)[2]+\
-               trans_rate_in(mul,mur)[3]      
-               #finally diagonal elements    
-    for i in range(0,3):
-         trans_rate[i,i] = - trans_rate.sum(axis=0)[i]
-         #imposing sum of p_i=1
-    trans_rate[3]=np.array([1,1,1,1])
-    return trans_rate
+#transition rates
+#\Gamma_{\alpa,j}^{+}
+#Gamma_{\alpha,j}^{-}=tun_{\alpha,j} -tun_{\alpha,j}
+fL = fermi_dirac(ed, VL).reshape(1, -1)
+fR = fermi_dirac(ed, VR).reshape(-1, 1)
+tun_Lg = (lg * semi_circle(ed, VL,w)).reshape(1, -1)*\
+    (fermi_dirac(ed, VL).reshape(1, -1))
+tun_Le = ( le * semi_circle(delta, VL,w))*\
+    fermi_dirac(delta, VL).reshape(1, -1)
+tun_Rg = ( rg * semi_circle(ed, VR,w))\
+    *fermi_dirac(ed, VR).reshape(1, -1)
+tun_Re = ( re * semi_circle(delta, VL,w))\
+    *fermi_dirac(delta, VR).reshape(1, -1)
 
 
+#\Gamma_{\alpha,electrons or holes} 
+gamma_le = gamma_re = np.zeros((4,4,len(VL),len(VL)))
+gamma_le[1,0] = gamma_le[3,2] = tun_Lg
+gamma_le[2,0] = gamma_le[3,1] = tun_Le
+gamma_re[1,0] = gamma_re[3,2] = tun_Rg
+gamma_re[2,0] = gamma_re[3,1] = tun_Re
 
-def populations(mul,mur):
-    populations = np.zeros((1,4))
-    b = np.array([0, 0,0,1])
-    populations = np.linalg.solve(trans_rate(mul, mur),b)
-    return populations
-#print(populations(1, 1))  
+#pending to implements symmetries here in order to save code-lines
+gamma_lh = gamma_rh = np.zeros((4,4,len(VL),len(VL)))
+gamma_lh[0,1] = gamma_lh[1,3] = tu_Lg-tun_Lg
+gamma_lh[0,2] = gamma_lh[2,3] = tu_Lg-tun_Le
+gamma_rh[0,1] = gamma_rh[1,3] = tu_Rg-tun_Rg
+gamma_rh[0,2] = gamma_rh[2,3] = tu_Re-tun_Re
+
+#print(gamma_le,gamma_lh)
 
 
-#Finally electronic current
-def I(mul,mur):
-    I = np.zeros((4,4)) 
-    #we start defining off diagonal elements
-    x = trans_rate_in(mul,mur)[0]-\
-         trans_rate_in(mul,mur)[2] 
-    I = np.sum(np.dot(x,populations(mul, mur)))
-    return I
+gamma = gamma_lh+gamma_rh+gamma_le+gamma_re
 
-print(I(1,0))
+#we need diagonal elements which are the sum of each column
+for i in range(0,3):
+    gamma[i,i] = -gamma.sum(axis=0)[0]
+#we apply the fact that \sum_i p_i=1
+gamma[3,:,:,:] = 1 
+#print(gamma)
+
+populations = b = np.zeros((4,1,len(VL),len(VL)))
+b[3,:,:,:]=1
+#populations = np.linalg.solve(gamma,b)
+
+print('p',populations)
+ 
+I = (gamma_le-gamma_lh)*populations
+
+print(gamma[:,:,1,1])
+
+#print(I(1,0))
 #visualization
 #plt.rc('text', usetex=True)
 #plt.rc('font', family='Bitstream Vera Serif', size=16)
 #fig = plt.figure(figsize=(8, 6))
 #axes = plt.axes()
 
-#VL = VR = np.linspace(-12, 12, 101)
-#VL, VR = np.meshgrid(VL, VR)
-
-
 # 2d visualization
 
+
+#VL, VR = np.meshgrid(VL, VR)
 #plt.contourf(VL ,VR, I(VL,VR),20,cmap='RdGy')
 #plt.colorbar(label = ''r'$I/e\Gamma$')
 #axes.set_xlabel(r'$eV_L$ ')
