d6c6b7f11766f71bd0393d0d0a183d7838dd5f18
“abejarano002” <abejaranosanchez@gmail.com>
2022-11-02 18:21:41 +0100

    package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Installation process
+
+#1. python setup.py sdsit
+#2. cd dist
+#3. pip install nanocavity-x.x.tar.gz
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='nanocavity',
+      description='Python package for light emission and quantum transport in plasmonic nanocavities',
+      author='Andres Bejarano',
+      author_email='andres.bejarano@dipc.org',
+      license='GPL',
+      version='0.1',
+      packages=['nanocavity'])
