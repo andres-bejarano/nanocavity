9cae6f38ea33503005bb564106acb2ce311156bd
“abejarano002” <abejaranosanchez@gmail.com>
2022-11-04 18:28:27 +0100

    corrected some bugs, pending to improve

--- nanocavity/distributions.py
+++ nanocavity/distributions.py
@@ -1,9 +1,20 @@
 import numpy as np
 
 #fermi-dirac distribution
-def fermi(E, T=10e-1, mu=0):
+def fermi(E, T=10e-2, mu=0):
     return 1. / (np.exp((E - mu) / T) + 1.)
 
 #bose-einstein distribution
 def bose(E, T=10e-1, mu=0):
     return 1. / (np.exp((E - mu) / T) - 1.)
+
+
+#fininte-band
+def semi_circle(e, mu, w):
+    e = np.array(e).reshape(-1, 1)
+    mu = np.array(mu).reshape(1, -1)
+    x = (e - mu) / w # broadcasting
+    x = np.clip(x, -1, 1) # values outside the band width should be set to zero
+    y = (1 - x ** 2) ** 0.5
+    return np.squeeze(y)
+
--- nanocavity/transition_rates.py
+++ nanocavity/transition_rates.py
@@ -2,6 +2,9 @@ import numpy as np
 from secondquant.composite import *
 from secondquant.operator import Operator
 from nanocavity.distributions import *
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+
 
 # E,v eigenvalues and eigenstates
 # d operator which interacts with the  bath
@@ -15,10 +18,9 @@ def transition_rate(E, v, d, G, mu):
     
     for i in range(len(d)):
         M[i]  = d[i].inner(v)
-
-    Mplus = np.einsum('iab,ij,jba->ab', M.conj(), G, M)
-    Gamma_plus = fermi(DE, mu) * \
-            np.repeat(Mplus.reshape(1, h, h), mu.shape[2], axis=0) 
-    Gamma_minus = (1-fermi(-DE, mu) * \
-            np.repeat(Mplus.conj().T.reshape(1, h, h), mu.shape[2], axis=0))
+    Mplus = np.einsum('iab,ij,jab->ab', M.conj(), G, M)
+    Gamma_plus = fermi(DE, mu=mu) * \
+            np.repeat(Mplus.conj().T.reshape(1, h, h), mu.shape[2], axis=0) 
+    Gamma_minus = (1-fermi(-DE, mu=mu)) * \
+            np.repeat(Mplus.reshape(1, h, h), mu.shape[2], axis=0)
     return Gamma_plus, Gamma_minus
