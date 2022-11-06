b18f72b097a87fc662c9ca885ffeefeff526ccf9
abejarano002 <abejaransanchez@gmail.com>
2022-11-06 22:34:50 +0100

    dissemination and description

--- nanocavity/transition_rates.py
+++ nanocavity/transition_rates.py
@@ -5,22 +5,62 @@ from nanocavity.distributions import *
 import numpy.linalg as la
 import matplotlib.pyplot as plt
 
+def operator_basis(A, v):
+    r""" Construction of an operator in given basis.
+    
+    Input variables:
 
-# E,v eigenvalues and eigenstates
-# d operator which interacts with the  bath
-# G tunneling rate between the bath and system
-def transition_rate(E, v, d, G, mu):
-    h = len(E)
-    G = G.reshape(2, 2)
+        - [A]: list of operators,
+        - [v]: numpy array with basis vectors.
+
+    Output variables:
+    
+        - [M]: numpy array with the information of each operator in A written in the basis of v.
+    """
+    v = np.array(v)
+    M = np.empty((len(A), len(v), len(v)))
+    for i in range(len(A)):
+          M[i]  = A[i].inner(v)
+    return M
+
+def fermi_matrix(E, mu):
+    r""" Construction of numpy array whose matrix elements are fermi functions evaluated for each energy differences and chemical potential.
+    
+    Input variables:
+
+        - [E]: all possible energy values,
+        - [mu]: all possible chemical potential energies.
+        
+    Output variables:
+    
+        - [fermi]: fermi function evaluated for all combination of input variables.
+    """
+    E = np.array(E)
     DE = E.reshape(1, -1, 1) - E.reshape(1, 1, -1)
     mu = mu.reshape(-1, 1, 1)
-    M = np.empty((len(d), h, h))
+    return fermi(E=DE, mu=mu)
+
+def transition_rate(E, v, A, g, mu=0):
+    r""" trasition_rate construct a matrix numpy array with all possible transition rates, where each matrix element represent the transition rate  between two states at given chemical potential.
+
+    Input variables
+
+        - [E]: system eigenvalues,
+        - [v]: system eigenvectors,
+        - [A]: list of all operators which interacts with the environment,
+        - [g]: list of all coupling values between each level and the environment, 
+        - [mu]: all possible chemical potential values.
+        
+    Output variables:
     
-    for i in range(len(d)):
-        M[i]  = d[i].inner(v)
-    Mplus = np.einsum('iab,ij,jab->ab', M.conj(), G, M)
-    Gamma_plus = fermi(DE, mu=mu) * \
-            np.repeat(Mplus.conj().T.reshape(1, h, h), mu.shape[2], axis=0) 
-    Gamma_minus = (1-fermi(-DE, mu=mu)) * \
-            np.repeat(Mplus.reshape(1, h, h), mu.shape[2], axis=0)
-    return Gamma_plus, Gamma_minus
+        - [Gp]: transition rate matrix for a transition in the system due to the injection of particles from the environment.
+        - [Gm]: transition rate matrix for a transition in the system due to the extraction of particles from the system.
+    """
+    g = np.array(g).reshape(2, 2)
+    fp = fermi_matrix(E, mu)
+    fm = 1 - fermi_matrix(-E, mu)
+    M = operator_basis(A, v)
+    G = np.einsum('iab,ij,jab->ab', M.conj(), g, M)
+    Gp = fp * G.conj().T
+    Gm = fm * G
+    return Gp, Gm
