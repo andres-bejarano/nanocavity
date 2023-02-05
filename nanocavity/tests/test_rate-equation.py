4e347a5ac90be6b80b2d0526a447f4c39f7598a1
abejarano002 <abejaransanchez@gmail.com>
2023-02-05 15:53:57 +0100

    fermi, rates and population tests

--- nanocavity/tests/test_rate-equation.py
+++ nanocavity/tests/test_rate-equation.py
@@ -5,68 +5,41 @@ from secondquant.operator import Operator
 from nanocavity.transition_rates import *
 from nanocavity.distributions import *
 
-def test_single_level():
-    gL = 0.8
-    gR = 0.2
-    V = np.linspace(0, 1, 1)    
-    #hamitlonian
-    d, Nf = composite(fermion_modes=1)
-    E, v = np.linalg.eigh(Nf[0].toarray())
-    #rates
-    GpL, GmL = transition_rate(E, v, d, gL , mu=V)
-    GpR, GmR = transition_rate(E, v, d, gR, mu=V)
-    GL, GR = transition_rate_matrix(GpL + GmL, GpR + GmR)
+def rate_parameters(modes=1):
+    d, Nf = composite(fermion_modes=modes)
+    if modes == 1: 
+        g = 0.2
+    else: 
+        g = np.arange(modes ** 2).reshape(modes, modes)
+    H = np.array(Nf).sum().toarray()
+    e, v = la.eigh(H)
+    return d, e, v, g
 
-    #populations
-    P = populations(GL + GR)
-    
-    #analytical result
-    f = fermi(E=1.,  mu=V)
-    P0 = (1 / (gL + gR)) * ((1 - f) * gL + (1 - f) * gR)
-    P1 = (1. / (gL + gR)) * (f * gL + f * gR)
-    PA = np.concatenate((P0, P1))
-    assert np.allclose(P, PA)
+def test_matrix_elements():
+    for i in range(1, 4):
+        d, e, v, g =  rate_parameters(i)
+        M = matrix_elements(d, v, g)
+        assert np.allclose(M.shape,(len(v), len(v)))
 
-    #electro-current
-    IL = electro_current(GpL, GmL, P)
-    IR = electro_current(GpR, GmR, P)
-    
-    #analytical reuslt
-    fL = f.reshape(1, -1)
-    fR = f.reshape(-1, 1)
-    IA = ((gL * gR) /(gL + gR)) * (fL - fR)
-    
-    assert np.allclose(IL, IA)
-    assert np.allclose(IR, IA)
+def test_fermi_matrix():
+    E = [0, 1, 2]
+    mu = np.linspace(-1, 1, 3)
+    A = fermi_matrix(E=E, mu=mu)  
+    DE = np.array(E).reshape(1, -1, 1) - np.array(E).reshape(1, 1, -1)
+    B = fermi(DE, mu=mu.reshape(-1, 1, 1))
+    assert np.allclose(A, B)
 
-def test_TLS():
-    V = np.linspace(-12, 12, 101)
-    N = len(V)
-
-    #leads-exciton couplings
-    gr = [[1,0], [0, 1]]
-
-    #Two level system operators
-    d, Nf = composite(fermion_modes=2)
-
-    #Hamiltonian diagonalization
-    delta = 1.
-    H = (delta * Nf[1]).toarray()
-    E, v = np.linalg.eigh(H)
-
-    #transition rates matrix
-    Gpr, Gmr = transition_rate(E, v, d, gr, mu=V)
-    GL, GR = transition_rate_matrix(Gpr + Gmr, Gpr + Gmr)
-
-    #populations
-    P = populations(GL + GR)
-
-    #current
-    I = electro_current(Gpr, Gmr, P)
-    
-    #analytical
-
-    pass
-
-def test_conservation_populations():
-    pass
+def test_transition_rate():
+    for i in range(1, 4):
+        d, e, v, g = rate_parameters(i)
+        Gp, Gm = transition_rate(e, v, d, g)
+        assert np.allclose(Gp[0].diagonal(), 0)
+        assert np.allclose(Gm[0].diagonal(), 0)
+        
+def test_populations():
+    for i in range(1, 4):
+        d, e, v, g = rate_parameters(i)
+        Gp, Gm = transition_rate(e, v, d, g)
+        GL, GR = transition_rate_matrix(Gp + Gm, Gp + Gm)
+        P = populations(GL + GR)
+        assert np.allclose(P[0, 0].sum(), 1)
