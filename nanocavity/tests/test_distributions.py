54cdef1cd21a059197e9d7f1630cc2eb4d763f06
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2023-02-07 12:01:23 +0100

    more tests

--- nanocavity/tests/test_distributions.py
+++ nanocavity/tests/test_distributions.py
@@ -8,10 +8,23 @@ def test_fermi():
     assert fermi(100, 0.1) == 0.0
 
 def test_fermi_arrays():
-    e = np.linspace(-1000, 1000, 10).reshape((2, 5))
+    e = np.linspace(-100, 100, 10).reshape((2, 5))
     nf = fermi(e, 1)
     assert nf.shape == e.shape
     nf = fermi(0, 1, e)
     assert nf.shape == e.shape
     nf = fermi(e, 1, e)
     assert nf.shape == e.shape
+
+def test_bose():
+    assert bose(0, 0.1) == np.inf
+    assert bose(100, 0.1) == 0.0
+
+def test_semi_circle():
+    e = np.linspace(-2, 2, 3)
+    sc = semi_circle(e, 0, 1)
+    assert sc[0] == 0
+    assert sc[1] == 1
+    assert sc[2] == 0
+    sc = semi_circle(e, mu=e, w=1)
+    assert sc.shape == (3, 3)
