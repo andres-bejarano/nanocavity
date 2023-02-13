d68a5153d7cec9ceee2b2170e38cc44b4abfffad
abejarano002 <abejaransanchez@gmail.com>
2023-02-13 12:26:40 +0100

    spaces

--- nanocavity/distributions.py
+++ nanocavity/distributions.py
@@ -10,7 +10,7 @@ def bose_einstein(E, kT=0.1, mu=0):
 
 #lorentzian
 def lorentzian(E, w):
-    return (1. / np.pi) * ((w / 2.) / ((w**2 / 4) + E**2))
+    return (1. / np.pi) * ((w / 2.) / ((w ** 2 / 4.) + E ** 2))
 
 #fininte-band
 def semi_circle(e, mu, w):
