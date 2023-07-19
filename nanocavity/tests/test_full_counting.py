352143d68dcbf7047dd83115006319754efa27a1
abejarano002 <abejaransanchez@gmail.com>
2023-07-19 16:10:18 +0200

    test for second field

--- nanocavity/tests/test_full_counting.py
+++ nanocavity/tests/test_full_counting.py
@@ -12,7 +12,7 @@ def jc_rates():
     H = H0 +  Hint
     e, v = H.eigh()
     VL = np.linspace(-2, 3, 11)
-    VR = np.linspace(-1, 2, 10)
+    VR = 0
     #electrodes transition rates
     GpL, GmL = transition_rate(e, v, [d1, d2], 1e-3*np.eye(2), mu=VL, kT=1e-2)
     GpR, GmR = transition_rate(e, v, [d1, d2], 1e-3*np.eye(2), mu=VR, kT=1e-2)
@@ -21,21 +21,26 @@ def jc_rates():
     #damping matrix
     Kp, Km = transition_rate(e, v, [a], 1, kT=1e-2, bath='bosonic')
     K = Kp + Km
-    return Kp, Km, GL, GR
+    return Kp, Km, GL, GpR, GmR
 
-def test_E_matrix():
-    Kp, Km, GL, GR = jc_rates()
-    E, x = E_matrix(Kp, Km, GL, GR, 1e-3)
+def test_M_matrix():
+    Kp, Km, GL, GpR, GmR = jc_rates()
+    E, x = M_matrix(Kp, Km, GL, GpR, GmR, 1e-3)
     index = x.size // 2
     #As the probability is conserved all eigenvalues at \chi= has to be <=0
-    assert np.all(np.round(np.real(E[index]), 10) <= 0) 
-
+    assert np.all(np.round(np.real(E[index, index]), 10) <= 0) 
+test_M_matrix()
 def test_cumulants():
-    Kp, Km, GL, GR = jc_rates()
-    I_fcs, _ = cumulants(Kp, Km, GL, GR, 1e-3)
-    
+    Kp, Km, GL, GpR, GmR = jc_rates()
+    I_fcs_ph, I_fcs_el, _, _, _ = cumulants(Kp, Km, GL, GpR, GmR, 1e-3)
+   
+    vr, k, _ = GpR.shape
+    GR = (GpR + GmR).reshape(1, vr, k, k)
     Gamma = (Kp + Km)[np.newaxis, np.newaxis] + GL + GR
     P = populations(Gamma)
-    I_re = photo_current(Kp, Km, P)
-
-    assert np.allclose(I_fcs, I_re)
+    Iph = photo_current(Kp, Km, P)
+    Iel = electro_current(GpR - GmR, P, electrode='right')
+    
+    assert np.allclose(I_fcs_ph, Iph)
+    assert np.allclose(I_fcs_el, Iel)
+test_cumulants()
