cae17d4d948d9cef21b0e8ff466ffca005a1011a
abejarano002 <abejaransanchez@gmail.com>
2023-11-10 15:06:46 +0100

    imports

--- article-stml-2023/populations.py
+++ article-stml-2023/populations.py
@@ -1,9 +1,9 @@
 import numpy as np
-from nanocavity.distributions import *
-from nanocavity.operators import *
-from nanocavity.transition_rates import *
+import nanocavity.distributions as ndist
+import nanocavity.operators as no
+import nanocavity.rate_equation as nre
 from qutip import (Qobj, about, basis, qeye, tensor, steadystate)
-
+import matplotlib.pyplot as plt
 
 Eg = 0.4
 delta = 0.9
@@ -18,28 +18,28 @@ VL = np.linspace(0, 2, 101)
 VR = 0
 
 #nanocav-populations
-Hnc, La, Ld = H_tls_nc(Eg, delta, omega, coupling)
+Hnc, La, Ld = no.H_tls_nc(Eg, delta, omega, coupling)
 Enc, Vnc = Hnc.eigh()
-GpL, GmL = transition_rate(Enc, Vnc, Ld, gamma, mu=VL, kT=kT)
-GpR, GmR = transition_rate(Enc, Vnc, Ld, gamma, mu=VR, kT=kT)
-GL, GR = transition_rate_matrix(GpL + GmL, GpR + GmR)
+GpL, GmL = nre.transition_rate(Enc, Vnc, Ld, gamma, mu=VL, kT=kT)
+GpR, GmR = nre.transition_rate(Enc, Vnc, Ld, gamma, mu=VR, kT=kT)
+GL, GR = nre.transition_rate_matrix(GpL + GmL, GpR + GmR)
 
 #damping matrix
-Kp, Km = transition_rate(Enc, Vnc, La, kappa, kT=kT, bath='bosonic')
+Kp, Km = nre.transition_rate(Enc, Vnc, La, kappa, kT=kT, bath='bosonic')
 K = Kp + Km
 
 #transtion rates matrix
 Gamma = K[np.newaxis, np.newaxis] + GL + GR
-Pnc = populations(Gamma)[:, 0, :]
+Pnc = nre.populations(Gamma)[:, 0, :]
 
 #QuTiP populations
-Hqt, L = H_tls_QuTiP(Eg, delta, omega,  coupling)
+Hqt, L = no.H_tls_QuTiP(Eg, delta, omega,  coupling)
 Eqt, Vqt = Hqt.eigenstates()
 
 def populations_qt(L, Hqt, Eqt, Vqt, VL, VR, kappa, gamma):
-    collapses_cavity = bosonic_collapses(L[0], E=Eqt, V=Vqt, kT=1e-2, k=kappa)
-    collapses_ground = fermionic_collapses(L[1], E=Eqt, V=Vqt, VL=VL, VR=VR, kT=1e-2, g=g)
-    collapses_excited = fermionic_collapses(L[2], E=Eqt, V=Vqt, VL=VL, VR=VR, kT=1e-2 ,g=g)
+    collapses_cavity = no.bosonic_collapses(L[0], E=Eqt, V=Vqt, kT=1e-2, k=kappa)
+    collapses_ground = no.fermionic_collapses(L[1], E=Eqt, V=Vqt, VL=VL, VR=VR, kT=1e-2, g=g)
+    collapses_excited = no.fermionic_collapses(L[2], E=Eqt, V=Vqt, VL=VL, VR=VR, kT=1e-2 ,g=g)
     c_ops = collapses_cavity + collapses_ground + collapses_excited
     A = steadystate(Hqt, c_ops).full()
     return  np.linalg.eigh(A)[0]
