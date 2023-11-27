b5005f7d85dff9462c7d2f2e8dc1152303689d3b
abejarano002 <abejaransanchez@gmail.com>
2023-11-27 11:18:01 +0100

    using np.gradient()

--- nanocavity/full_counting.py
+++ nanocavity/full_counting.py
@@ -1,27 +1,7 @@
 import numpy as np
 
-
-def d1(E, zero, p=1e-3):
-    L = (E[zero] - E[zero-1]) / p
-    R = (E[zero+1] - E[zero]) / p
-    M = (E[zero+1] - E[zero-1]) / (2*p)
-    return L, R, M
-
-def d2(E, zero, p=1e-3):
-    L = (E[zero] + E[zero-2] - 2 * E[zero-1]) / p ** 2
-    R = (E[zero+2] + E[zero] - 2 * E[zero+1]) / p ** 2
-    M = (E[zero+2] + E[zero-2] - 2 * E[zero]) / (4 * p ** 2)
-    return L, R, M
-
-def dxy(E, zero, p=1e-3):
-    L = (E[zero-1, zero-1] + E[zero, zero] - E[zero, zero-1] - E[zero-1, zero]) / p ** 2
-    R = (E[zero+1, zero+1] + E[zero, zero] - E[zero, zero+1] - E[zero+1, zero]) /  p ** 2
-    M = (E[zero+1, zero+1] + E[zero-1, zero-1] - E[zero+1, zero-1] - E[zero-1, zero+1]) / (4 * p ** 2)
-    return L, R, M
-
-
 def M_matrix(Kp, Km, GpL, GmL, GpR, GmR, p=1e-3, ninter=10):
-    x = np.linspace(-5*p, 5*p, ninter + 1)
+    x = p * np.linspace(-2, 2, 5)
     
     #K[dim(h), dim(h)]
     K = Kp + Km
@@ -81,23 +61,26 @@ def E_max(Kp, Km, GpL, GmL, GpR, GmR, p=1e-3, ninter=10):
     nxy = np.repeat(ny[np.newaxis], len(x), axis=0)
     #nx[len(y),vl, vr] and nxy[len(x), len(y), vl, vr]
     E_max = np.take_along_axis(E, np.expand_dims(nxy, axis=4), axis=4)[:, :, :, :, 0]
-    return E_max, zero
+    return E_max, zero, x
 
 def cumulants(Kp, Km, GpL, GmL, GpR, GmR, p, ninter=10):
-    E, zero = E_max(Kp, Km, GpL, GmL, GpR, GmR, p, ninter)
+    E, zero, x = E_max(Kp, Km, GpL, GmL, GpR, GmR, p, ninter)
     #E[x, y, vl, vr]
-    E_re = np.real(E)
-    E_im = np.imag(E)
+    dx = np.gradient(E, x, axis=0)
+    dy = np.gradient(E, x, axis=1)
+    dxx = np.gradient(dx[:, zero, :, :], x, axis=0)[zero]
+    dyy = np.gradient(dy[zero, :, :, :], x, axis=0)[zero]
+    dxy = np.gradient(dx[zero, :, :, : ], x, axis=0)[zero]
 
     #the first cumulants is the gradient respect to x and y
     #the first cumulants are the average number of particles 
     #per unit T that get in the drain.
-    _, _, Nph = d1(E_im[:, zero, :, :], zero, p)
-    _, _, Nel = d1(E_im[zero, :, :], zero, p)
+    Iph = np.imag(dx[zero, zero])
+    Iel = -np.imag(dy[zero, zero])
     
-    #second cumulant Z = sigma2
-    _, _, Zph = d2(E_re[:, zero, :, :], zero, p)
-    _, _, Zel = d2(E_re[zero, :, :, :], zero, p)
-    _, _, covariance = dxy(E_re, zero, p)
+    #second cumulant Z = sigma2/T
+    Zph = -np.real(dxx)
+    Zel = -np.real(dyy)
+    covariance = -np.real(dxy)
     
-    return Nph, Nel, -Zph, -Zel, -covariance
+    return Iph, Iel, Zph, Zel, covariance
--- nanocavity/tests/test_full_counting.py
+++ nanocavity/tests/test_full_counting.py
@@ -25,56 +25,16 @@ def test_M_matrix():
     #As the probability is conserved all eigenvalues at \chi= has to be <=0
     assert np.all(np.round(np.real(E[index, index]), 10) <= 0) 
 
-#its also a test for derivates
-#the real part of E is linear in a range of 1e-6
-#whereas the real part is parabolic in 1e-9
-def test_E_max():
-    p = 1e-15
-    n = 12
-    Kp, Km, GpL, GmL, GpR, GmR = jc_rates()
-    E, zero = nfcs.E_max(Kp, Km, GpL, GmL, GpR, GmR, p=p, ninter=n)
-    E_re = np.real(E)
-    E_im = np.imag(E)
-    
-    NphL, NphR, NphM = nfcs.d1(E_im[:, zero, :, :], zero)
-    
-    assert np.allclose(NphL, NphR)
-    assert np.allclose(NphL, NphM)
-
-
-    NelL, NelR, NelM = nfcs.d1(E_im[zero, :, :, :], zero)
-
-    assert np.allclose(NelL, NelR)
-    assert np.allclose(NelL, NelM)
-    
-    ZphL, ZphR, ZphM = nfcs.d2(E_re[:, zero, :, :], zero, p=p)
-
-    assert np.allclose(ZphL, ZphR)
-    assert np.allclose(ZphL, ZphM)
-    
-    ZelL, ZelR, ZelM = nfcs.d2(E_re[zero, :, :, :], zero)
-
-    assert np.allclose(ZelL, ZelR)
-    assert np.allclose(ZelL, ZelM)
-
-
-
-    covL, covR, covM = nfcs.dxy(E_re, zero)
-
-    assert np.allclose(covL, covR)
-    assert np.allclose(covL, covM)
 
 
 def test_cumulants():
     Kp, Km, GpL, GmL, GpR, GmR = jc_rates()
-    Nph, Nel, _, _, _ = nfcs.cumulants(Kp, Km, GpL, GmL, GpR, GmR, p=1e-9)
+    Iphfcs, Ielfcs, _, _, _ = nfcs.cumulants(Kp, Km, GpL, GmL, GpR, GmR, p=1e-9)
     GL = (GpL + GmL)[:, None]  # VL, VR
     GR = (GpR + GmR)[None, :]
     Gamma = (Kp + Km)[np.newaxis, np.newaxis] + GL + GR
     P = nre.populations(Gamma)
-    Iph = nre.photo_current(Kp, Km, P)
-    Iel = -nre.electro_current(GpR - GmR, P, electrode='right')
-    #we are counting the number of electrons that get in the right electrode
-    #then we have compare with -Iel to remove the charge '-e'
-    assert np.allclose(Nph, Iph)
-    assert np.allclose(Nel, Iel)
+    Iphnc = nre.photo_current(Kp, Km, P)
+    Ielnc = nre.electro_current(GpR - GmR, P, electrode='right')
+    assert np.allclose(Iphfcs, Iphnc)
+    assert np.allclose(Ielfcs, Ielnc)
