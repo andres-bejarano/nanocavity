34a715bd260ca1c794672634e85e13ffd2576da8
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2024-01-01 15:22:35 +0100

    handle different environments in install script

--- install.sh
+++ install.sh
@@ -2,9 +2,23 @@
 d=`pwd`
 
 # install
-python3 -m pip install . --user
+if [ -z "$CONDA_DEFAULT_ENV" ]; then
+    echo "No Conda environment is active, installing with --user."
+    python3 -m pip install . --user
+else
+    echo "Conda environment '$CONDA_DEFAULT_ENV' is active."
+    python3 -m pip install .
+fi
 
 # run tests
 cd ~
-pytest-3 --pyargs nanocavity
+if command -v pytest > /dev/null; then
+    # pytest is available
+    pytest --pyargs nanocavity
+elif command -v pytest-3 > /dev/null; then
+    # pytest-3 is available
+    pytest-3 --pyargs nanocavity
+else
+    echo "Neither pytest-3 nor pytest is available. Please install pytest."
+fi
 cd $d
