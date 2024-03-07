6bdb55aee3a414ab3bd803bd2f29e2ee42d195ad
“abejarano002” <abejaranosanchez@gmail.com>
2024-03-07 15:04:38 +0100

    new method stable respect to the variaton of bias voltage

--- nanocavity/master_equation.py
+++ nanocavity/master_equation.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.linalg import eig
 import nanocavity.operators as no
-
+from qutip import operator_to_vector, steadystate
 
 def current(J, L):
     J = J.full()
@@ -32,28 +32,58 @@ def cumulants(H, S_op, VL, VR,  kT=1e-2, kappa=0.1, gL=1e-3, gR=1e-3, m=0, iva=F
     Ze = -np.gradient(np.gradient(np.real(Emax_xy[2, :]), x), x)[2]
     return Ig, Ie, Zg, Ze
 
-def noise(L, Jin, Jout, wlist, method='eigen'):
+def noise(L, Jin, Jout, wlist, method='direct'):
     J1 = Jout.full() - Jin.full()
-    J2 =  Jout.full() + Jin.full()
+    J2 = Jout.full() + Jin.full()
 
-    #w/v left/right eigenvectors
-    El, vl, vr = eig(L.full(), left=True)
+    if method=='direct':
+        rho_ss = steadystate(L)
+        d = rho_ss.shape[0]
+        rho_ss = operator_to_vector(rho_ss).full()
+        L = L.full()
+        d2 = L.shape[0]
+        J = J2
+        Id = np.eye(d2, d2)
+        #we have to solve tr{ALB^-1C} 
+        #A = J' = J - Tr{J * rho_st}
+        Jtr = np.dot(J, rho_ss).reshape(d, d).trace()
+        if Jtr == 0:
+            return 0
+        else:
+            A = J - Jtr * Id
+            #C = J' rho_st
+            C = np.dot(A, rho_ss)
+
+            #we can avoid inverting B: B^-1 C = W 
+            #BW = C  we can compute W as:
+            #B = L^2 + \omega^2
+            S = np.zeros(len(wlist))
+            for i, omega in enumerate(wlist):
+                B = np.dot(L, L) + Id * omega ** 2
+                W = np.linalg.solve(B, C)
+                S[i] = np.real(np.dot(A, np.dot(L, W)).reshape(d, d).trace())
+            return  Jtr - 2 * S
+
+    elif method=='eigen':
+        print('The stability of this method it is not guaranteed')
+        #w/v left/right eigenvectors
+        El, vl, vr = eig(L.full(), left=True)
 
 
-    norm = np.einsum("ai,ai->i", vl.conj(), vr) ** -0.5
-    vl *= norm
-    vr *= norm
+        norm = np.einsum("ai,ai->i", vl.conj(), vr) ** -0.5
+        vl *= norm
+        vr *= norm
 
-    index = np.argmin(np.abs(El))
-    J2tr =  np.dot(vl[:, index], np.dot(J2, vr[:, index]))
+        index = np.argmin(np.abs(El))
+        J2tr =  np.dot(vl[:, index], np.dot(J2, vr[:, index]))
     
-    S = np.zeros(len(wlist))
-    for j, w in enumerate(wlist):
-        Sj = 0
-        for i, Ei in enumerate(El[1:]):
-            num =  Ei * (np.dot(vl[:, 0].T.conj(), np.dot(J1, vr[:, i]))) \
-                * (np.dot(vl[:, i].T.conj(), np.dot(J1, vr[:, 0])))
-            den = w ** 2 + np.abs(Ei) ** 2    
-            Sj += np.real(num)/den
-        S[j] = Sj
-    return J2tr + 2 * S
+        S = np.zeros(len(wlist))
+        for j, w in enumerate(wlist):
+            Sj = 0
+            for i, Ei in enumerate(El[1:]):
+                num =  Ei * (np.dot(vl[:, 0].T.conj(), np.dot(J1, vr[:, i]))) \
+                          * (np.dot(vl[:, i].T.conj(), np.dot(J1, vr[:, 0])))
+                den = w ** 2 + np.abs(Ei) ** 2    
+                Sj += num/den
+            S[j] = np.real(Sj)
+        return J2tr - 2 * S
