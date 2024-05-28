3f5890de37970ce723a861d1bbd8e0e8e780601f
abejarano002 <abejaransanchez@gmail.com>
2024-05-28 16:26:43 +0200

    no full

--- nanocavity/master_equation.py
+++ nanocavity/master_equation.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.linalg import eig
 
 def eig_norm(L):
-    El, vl, vr = eig(L.full(), left=True)
+    El, vl, vr = eig(L, left=True)
     norm = np.einsum("ai,ai->i", vl.conj(), vr) ** -0.5
     vl *= norm
     vr *= norm
