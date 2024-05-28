abe54431e0bd79ebb6f4c6e942b33ff00bb31160
abejarano002 <abejaransanchez@gmail.com>
2024-05-28 18:20:42 +0200

    2

--- nanocavity/tests/test_master_equation.py
+++ nanocavity/tests/test_master_equation.py
@@ -12,9 +12,10 @@ from qutip import steadystate, liouvillian
 Eg = 0.4
 delta = 0.9
 omegac = 1
-coupling = 0.3
 
 H_parameters = Eg, delta, omegac, coupling 
+coupling = 0.5
+u = 1.2
 
 #bath parameters
 gL = 1e-3
@@ -25,9 +26,6 @@ kT = 0.1
 
 
 def fcs(VL=3, VR=-3):
-    Hnc, [Dg, De, A] = no.H_tls(Eg, delta, omegac, coupling)
-    Enc, Vnc = Hnc.eigh()
-
     #transtion rates, populations and spectrum
     Kp, Km = nre.transition_rate(Enc, Vnc,  A, kappa, kT=kT, bath='bosonic')
     GpL, GmL = nre.transition_rate(Enc, Vnc, [Dg, De], gL*np.eye(2), mu=VL, kT=kT)
@@ -36,7 +34,7 @@ def fcs(VL=3, VR=-3):
     GL = (GpL + GmL)[:, None]  # VL, VR
     GR = (GpR + GmR)[None, :]
     K = Kp + Km
-    Gamma = K[np.newaxis, np.newaxis] + GL + GR 
+    Gamma = K[np.newaxis, np.newaxis] + GL + GR
     P_re = nre.populations(Gamma)
     return Ig_re, -Ie_re, Zg_re, Ze_re
 
@@ -62,7 +60,68 @@ def test_current():
             Ig_me = qme.current(qo.jump(Cm) - qo.jump(Cp), L)
             Ie_me = qme.current(qo.jump(CpL) - qo.jump(CmL), L)
     
-            Ig_re, Ie_re, _, _ = fcs(VL, VR)
+    return P_re, Ig_re, Ie_re, Zg_re, Ze_re
+
+def test_eigenoperator():
+    for AA in [Dg, De, A]:
+        #if we sum all eigenoperators we need to recover Dg
+        #ij are the matrix elements of each eigenoperator
+        A_eigen = nme.eigen_operator(AA.toarray(), Vnc)
+        A_eigen_sum = np.einsum('ijkp->ij', A_eigen)
+        assert np.allclose(A_eigen_sum, AA.toarray())
+
+
+def test_Liouvillian():
+    H1 = Hnc - u * Dg.d * De.d * De * Dg
+    H2 = Hqt -u * dg.dag() * de.dag() * de * dg
+
+    for meth in ('einsum', 'kron'):
+        Lnc = nme.liouvillian(H1, [Dg, De, A], gL, gR, kappa, method=meth)
+        Lqt = no.Liouvillian(H2, [dg, de, a], 10, -10, kT=1e-2,gL=gL, gR=gR)
+
+        Enc, _ = np.linalg.eig(Lnc)
+        Eqt, _ = np.linalg.eig(Lqt.full())
+
+        E1real = np.sort(Enc.real)
+        E2real = np.sort(Eqt.real)
+
+        E1imag = np.sort(Enc.imag)
+        E2imag = np.sort(Eqt.imag)
+
+        E1abs = np.sort(abs(Enc))
+        E2abs = np.sort(abs(Eqt))
+
+        for i in range(len(Enc)):
+            assert np.allclose(E1real, E2real)
+            assert np.allclose(E1imag, E2imag)
+            assert np.allclose(E1abs, E2abs)
+
+test_Liouvillian()
+
+def test_current():
+    for VL in [-3, -2.1, -1.1, 2]:
+        for VR in [-1.3, 1.4, 2.5]:
+
+            VLR = VL - VR
+            VRL = VR - VL
+            L = Li(VL, VR)
+
+            Jtip_out = gL * no.jump_fermionic(dg.dag(), Eqt, Vqt, VL, kT, rate='in') + \
+                       gL * no.jump_fermionic(de.dag(), Eqt, Vqt, VL, kT, rate='in') + \
+                        m * no.dissipator_lead(a, Eqt, Vqt, eV=VLR, kT=kT, rate='out') + \
+                        m * no.dissipator_lead(a.dag(), Eqt, Vqt, eV=VLR, kT=kT, rate='in')
+
+
+            Jtip_in = gL * no.jump_fermionic(dg, Eqt, Vqt, VL, kT, rate='out') + \
+                      gL * no.jump_fermionic(de, Eqt, Vqt, VL, kT, rate='out') + \
+                       m * no.dissipator_lead(a, Eqt, Vqt, eV=VRL, kT=kT, rate='out') + \
+                       m * no.dissipator_lead(a.dag(), Eqt, Vqt, eV=VRL, kT=kT, rate='in')
+
+            Ja_out = kappa * no.jump_bosonic(a, Eqt, Vqt, kT, rate='out')
+            Ja_in = kappa * no.jump_bosonic(a.dag(), Eqt, Vqt, kT, rate='in')
+
+            Ig_me = nme.current(Ja_out - Ja_in, L)
+            Ie_me = nme.current(Jtip_in - Jtip_out, L)
 
             assert np.allclose(Ig_me, Ig_re)
             assert np.allclose(Ie_me, Ie_re)
@@ -91,3 +150,31 @@ def test_noise():
             _, _, Zg_re, Ze_re = fcs(VL, VR)
             assert np.allclose(Zg_me, Zg_re, atol=1e-7)
             assert np.allclose(Ze_me, Ze_re, atol=1e-7)
+def test_cumulants():
+   #nme.cumulants is too slow we avoid to sweep the bias
+    _, Ig_re, Ie_re, Zg_re, Ze_re = fcs(3, -3)
+    Ig_me, Ie_me, Zg_me, Ze_me = nme.cumulants(Hqt, [dg, de, a], 3, -3, kT=kT, gL=gL, gR=gR)
+    assert np.allclose(Ig_me, Ig_re)
+    assert np.allclose(Ie_me, Ie_re)
+    #assert np.allclose(Zg_me, Zg_re, atol=1e-7)
+    #assert np.allclose(Ze_me, Ze_re, atol=1e-6)
+
+def test_noise():
+    Ja_out = kappa * no.jump_bosonic(a, Eqt, Vqt, kT, rate='out')
+    Ja_in = kappa * no.jump_bosonic(a.dag(), Eqt, Vqt, kT, rate='in')
+
+    for VL in [-3, -2.1, -1.1, 2]:
+        for VR in [-1.3, 1.4, 2.5]:
+            L = Li(VL, VR)
+
+            Jtip_in = gL * no.jump_fermionic(dg.dag(), Eqt, Vqt, VL, kT, rate='in') + \
+                      gL * no.jump_fermionic(de.dag(), Eqt, Vqt, VL, kT, rate='in')
+
+            Jtip_out = gL * no.jump_fermionic(dg, Eqt, Vqt, VL, kT, rate='out') + \
+                       gL * no.jump_fermionic(de, Eqt, Vqt, VL, kT, rate='out')
+
+            Zg_me = nme.noise(L, Ja_in, Ja_out)
+            Ze_me = nme.noise(L, Jtip_in, Jtip_out)
+            _, _, _, Zg_re, Ze_re = fcs(VL, VR)
+            assert np.allclose(Zg_me, Zg_re, atol=1e-6)
+            assert np.allclose(Ze_me, Ze_re, atol=1e-6)
