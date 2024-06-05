- sign wrong

nanocavity/master_equation.py
@@ -74,7 +74,7 @@ def spectrum(L, a, wlist):
         Bk = np.dot(vl[:, k].conj(), A.dot(Rho_st))
         if abs(Ak) > 1e-12:
             print(f"{k:4} {np.abs(Ak):12.6f} {np.abs(Bk):12.6f} {np.abs(Ak * Bk):12.6f} {El[k].real:12.6f} {El[k].imag:12.6f}")
-            Dist = ndist.lorentzian(wlist + El[k].imag, - 2 * El[k].real)
+            Dist = ndist.lorentzian(wlist - El[k].imag, - 2 * El[k].real)
             I += Ak * Bk * Dist
     return I.real
 

nanocavity/operators.py
@@ -100,7 +100,7 @@ def liouvillian(H, c_ops, method='kron'):
         L = -1j * (np.einsum('ik,jl->ijkl', H, Id) -  1j * np.einsum('ki,lj->ijkl', Id, H))
         return np.reshape(L, (dim ** 2, dim ** 2))
     elif method=='kron':
-        L = -1j * (np.kron(Id, H) - np.kron(H, Id))
+        L = 1j * (np.kron(Id, H) - np.kron(H, Id))
 
     L += dissipator(c_ops, method)
     