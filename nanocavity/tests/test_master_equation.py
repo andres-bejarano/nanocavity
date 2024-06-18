02ba91d0da46399e671fdf343335ca880af51a09
“abejarano002” <abejaranosanchez@gmail.com>
2024-06-18 20:55:30 +0200

    test

--- nanocavity/tests/test_master_equation.py
+++ nanocavity/tests/test_master_equation.py
@@ -7,6 +7,7 @@ import nanocavity.qutip.master_equation as qme
 import nanocavity.full_counting as nfc
 from scipy.linalg import eig
 import qutip as qt
+import nanocavity.tls as tls
 
 #system parameters
 Eg = 0.4
@@ -23,22 +24,10 @@ kappa = 0.1
 m = 1e-4
 kT = 0.1
 
-Hqt, [dg, de, a] = qo.H_tls(Eg, delta, omegac, coupling)
-Eqt, Vqt =  Hqt.eigenstates()
-
-
-Hnc, [Dg, De, A] = no.H_tls(Eg, delta, omegac, coupling)
-Enc, Vnc = Hnc.eigh()
-
-def system(pack):
-    if pack=='nanocavity':
-        return no.H_tls(Eg, delta, omegac, coupling)
-    elif pack=='qutip':
-        return qo.H_tls(Eg, delta, omegac, coupling)
-
 def fcs(VL=3, VR=-3):
     #transtion rates, populations and spectrum
-    Hnc, [Dg, De, A] = no.H_tls(Eg, delta, omegac, coupling)
+    H_parameters = Eg, delta, omegac, coupling
+    Hnc, [Dg, De, A] = tls.Hamiltonian('nanocavity', *H_parameters)
     Enc, Vnc = Hnc.eigh()
     Kp, Km = nre.transition_rate(Enc, Vnc,  A, kappa, kT=kT, bath='bosonic')
     GpL, GmL = nre.transition_rate(Enc, Vnc, [Dg, De], gL*np.eye(2), mu=VL, kT=kT)
@@ -77,36 +66,35 @@ def test_eig_norm():
 def test_correlation_AB():
     tlist = np.linspace(0., 200, 10001)
     for coupling in [0.005, 0.05, 0.5]:
-        Hnc, [Dg, De, A] = system('nanocavity')
-        Hqt, [dg, de, a] = system('qutip')        
+        Hnc, [Dg, De, A] = tls.Hamiltonian('nanocavity', Eg, delta, omegac, coupling)
+        Hqt, [dg, de, a] = tls.Hamiltonian('qutip', Eg, delta, omegac, coupling)        
         H_parameters = Eg, delta, omegac, coupling
         VL, VR = 3, -3
         for iva in (False, True):
-            c_nc = no.collapses_tls(H_parameters, VL, VR, kappa, gL, gR, kT, iva=iva)
+            c_nc = tls.collapses('nanocavity', H_parameters, VL, VR, kappa, gL, gR, kT, iva=iva)
             L = no.liouvillian(Hnc, list(c_nc))
             Snc = nme.correlation_AB(L, A.d, A, tlist)
 
-            c_qt = qo.collapses_tls(H_parameters, VL, VR, kappa, gL, gR, kT, iva=iva)
+            c_qt = tls.collapses('qutip', H_parameters, VL, VR, kappa, gL, gR, kT, iva=iva)
             rho_st= qt.steadystate(Hqt, list(c_qt))
             Sqt = qt.correlation_2op_1t(H=Hqt, state0=rho_st, taulist=tlist, c_ops=list(c_qt), a_op=a.dag(), b_op=a)
-            print(iva, coupling)
             assert np.allclose(Snc.real, Sqt.real, atol=1e-5)
             assert np.allclose(Snc.imag, Sqt.imag, atol=1e-5)
 
 def test_spectrum():
     wlist = np.linspace(0., 1.8, 100003)
-    Inc = nme.spectrum_tls('nanocavity', H_parameters, 3, -3, kappa, gL, gR, kT, wlist)
-    Iqt = nme.spectrum_tls('qutip', H_parameters, 3, -3, kappa, gL, gR, kT, wlist)
+    Inc = tls.spectrum('nanocavity', H_parameters, 3, -3, kappa, gL, gR, kT, wlist)
+    Iqt = tls.spectrum('qutip', H_parameters, 3, -3, kappa, gL, gR, kT, wlist)
     assert np.allclose(Inc, Iqt)
 
 def test_current():
     for VL, VR in [[3, -3], [2, 0], [-1, 2]]:
-            [dg, de, a], Hqt, c_ops = qo.collapses_tls(H_parameters, VL, VR, kappa, gL, gR, kT, alone=False)
+            [dg, de, a], Hqt, c_ops = tls.collapses('qutip', H_parameters, VL, VR, kappa, gL, gR, kT, alone=False)
             _, Vqt = Hqt.eigenstates()
 
 
             #left electrode
-            cp_gL, cm_gL = qo.collapses(dg, Hqt, kT, bath='fermionic', mu=VL, total=False)
+            cp_gL, cm_gL = qo.collapses( dg, Hqt, kT, bath='fermionic', mu=VL, total=False)
             cp_eL, cm_eL = qo.collapses(de, Hqt, kT, bath='fermionic', mu=VL, total=False)
             CpL = list(np.sqrt(gL) * np.array(cp_gL + cp_eL))
             CmL = list(np.sqrt(gL) * np.array(cm_gL + cm_eL))
@@ -127,7 +115,7 @@ def test_current():
 
 def test_noise():
     for VL, VR in [[-3, 0], [-2.1, 1], [0, 3]]:
-            [dg, de, a], Hqt, c_ops = qo.collapses_tls(H_parameters, VL, VR, kappa, gL, gR, kT, alone=False)
+            [dg, de, a], Hqt, c_ops = tls.collapses('qutip', H_parameters, VL, VR, kappa, gL, gR, kT, alone=False)
             _, Vqt = Hqt.eigenstates()
 
 
@@ -157,6 +145,6 @@ def test_g2():
     delta = 0.99
     H_parameters = Eg, delta, omegac, coupling
     tlist = np.linspace(0., 3000, 10)
-    g2nc = nme.g2_tls('nanocavity', H_parameters, VL, VR, kappa, gL, gR, kT, tlist)
-    g2qt = nme.g2_tls('qutip', H_parameters, VL, VR, kappa, gL, gR, kT, tlist)
+    g2nc = tls.g2('nanocavity', H_parameters, VL, VR, kappa, gL, gR, kT, tlist)
+    g2qt = tls.g2('qutip', H_parameters, VL, VR, kappa, gL, gR, kT, tlist)
     assert np.allclose(g2nc, g2qt, atol=1e-1)
