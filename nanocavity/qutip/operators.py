55adecceee15b343d6baee5b7610fa94e7a0776b
“abejarano002” <abejaranosanchez@gmail.com>
2024-06-18 20:55:10 +0200

    removing

--- nanocavity/qutip/operators.py
+++ nanocavity/qutip/operators.py
@@ -2,27 +2,6 @@ import numpy as np
 import nanocavity.distributions as ndist
 from qutip import qeye, tensor, destroy, sprepost, vector_to_operator, operator_to_vector, spre, spost, fdestroy, sigmaz, lindblad_dissipator
 
-def H_tls(Eg, delta, omega, coupling, u=0, rwa=True, max_bosons=1):
-    N = max_bosons + 1
-    dg = tensor(fdestroy(2, 0), qeye(N))
-    de = tensor(fdestroy(2, 1), qeye(N))
-
-    #sigmaz = [[1, 0], [0, -1]] is playing the role of permutation as
-    # |n_g, n_e> = -|n_e, n_g>
-    a = tensor(sigmaz(), sigmaz(), destroy(N))
-
-    He = Eg * dg.dag() * dg + (Eg + delta)* de.dag() * de +  u * dg.dag() * de.dag() * de * dg
-    Hp = omega * a.dag() * a
-    H0 = He + Hp
-    if rwa:
-        Hint = coupling * (a.dag() * dg.dag() * de + a * de.dag() * dg)
-    else:
-        Hint = coupling * (a + a.dag()) * (dg.dag() * de + de.dag() * dg)
-    H = H0 + Hint
-    E, V = H.eigenstates()
-    L = [dg, de, a]
-    return H, L
-
 def collapses(A_op, H, kT, bath, mu=0, total=True, cutoff=1e-12):
     """
     This script describes the collapse operator defined below. We will have an operator A_op of the system that interacts with a given bath, which is in thermal equilibrium and will be characterized by the Fermi-Dirac or Bose-Einstein function depending on its nature.
@@ -83,42 +62,6 @@ def collapses(A_op, H, kT, bath, mu=0, total=True, cutoff=1e-12):
     if total:
         return cp + cm
     return cp, cm
-
-
-def collapses_tls(H_parameters, VL, VR, kappa, gL, gR, kT, m=0, lead2lead=False, alone=True, iva=False):
-
-    H, [dg, de, a] = H_tls(*H_parameters)
-
-    if iva:
-        coupling = H_parameters[3] 
-        Hint = coupling * (a.dag() * dg.dag() * de + a * de.dag() * dg)
-        H -= Hint
-    #left electrode
-    c_gL = collapses(dg, H, kT, bath='fermionic', mu=VL)
-    c_eL = collapses(de, H, kT, bath='fermionic', mu=VL)
-    CL = np.sqrt(gL) * np.array(c_gL + c_eL)
-
-    #right electrode
-    c_gR = collapses(dg, H, kT, bath='fermionic', mu=VR)
-    c_eR = collapses(de, H, kT, bath='fermionic', mu=VR)
-    CR = np.sqrt(gR) * np.array(c_gR + c_eR)
-
-    #cavity mode
-    CA = collapses(a, H, kT, bath='bosonic')
-
-    CA = np.sqrt(kappa) * np.array(CA)
-
-    if lead2lead:
-        c_lead = lead_cavity_lead_collapses(a, H, VL, VR, kT, m)
-        c_ops = np.concatenate((CL, CR, CA, c_lead))
-    else:
-        c_ops = np.concatenate((CL, CR, CA))
-
-    if alone:
-        return c_ops
-    if iva:
-        return [dg, de, a], H + Hint, c_ops
-    return [dg, de, a], H, c_ops
 
 def lead_cavity_lead_collapses(A_op, H, VL, VR, kT, m):
     """
