Fix for overflow in FD and BE distributions

nanocavity/tests/test_distributions.py
@@ -2,13 +2,14 @@ import numpy as np
 from nanocavity.distributions import *
 
 
-def test_fermi_dirac(): 
-    assert fermi_dirac(-100) == 1.0
+def test_fermi_dirac():
     assert fermi_dirac(0) == 0.5
-    assert fermi_dirac(100) == 0.0
-    assert fermi_dirac(-100, 0.1) == 1.0
-    assert fermi_dirac(0, 0.1) == 0.5
-    assert fermi_dirac(100, 0.1) == 0.0
+    e = [-1e5, -100, 0, 100, 1e5]
+    nf = [1, 1, 0.5, 0, 0]
+    assert np.allclose(fermi_dirac(e), nf)
+    assert np.allclose(fermi_dirac(e, 0.1), nf)
+    assert np.allclose(fermi_dirac(e, 0.01), nf)
+    assert not np.allclose(fermi_dirac(e, 100), nf)
 
 def test_fermi_dirac_arrays():
     e = np.linspace(-100, 100, 10).reshape((2, 5))
@@ -24,10 +25,18 @@ def test_fermi_dirac_arrays():
     assert nf.shape == e.shape
 
 def test_bose_einstein():
-    assert bose_einstein(0) == np.inf
-    assert bose_einstein(100) == 0.0
-    assert bose_einstein(0, 0.1) == np.inf
-    assert bose_einstein(100, 0.1) == 0.0
+    assert bose_einstein(1e5) < 1e-100
+    e = [-1e5, -100, 0, 100, 1e5]
+    nb = [-1, -1, np.inf, 0, 0]
+    assert np.allclose(bose_einstein(e), nb)
+    assert np.allclose(bose_einstein(e, 0.1), nb)
+    assert np.allclose(bose_einstein(e, 0.01), nb)
+    assert not np.allclose(bose_einstein(e, 100), nb)
+    
+def test_bose_einstein_arrays():
+    e = np.linspace(-100, 100, 10).reshape((2, 5))
+    nb = bose_einstein(e)
+    assert nb.shape == e.shape
 
 def test_bath_dist():
     for bath in ['bosonic', 'fermionic', 'leadtolead']: