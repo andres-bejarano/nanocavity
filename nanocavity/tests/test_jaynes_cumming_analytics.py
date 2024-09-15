e863cdd620988341cc0aca2395bf40bbee84603b
“abejarano002” <abejaranosanchez@gmail.com>
2024-09-15 19:12:18 +0200

    more tests

--- nanocavity/tests/test_jaynes_cumming_analytics.py
+++ nanocavity/tests/test_jaynes_cumming_analytics.py
@@ -17,3 +17,28 @@ def test_Epm():
     Em, Ep = jc.Emp((1, 2, 3, 4), 5) 
     OmegaR = jc.Omega_R(2, 3, 4, 5)
     assert np.allclose((Ep - Em), OmegaR)
+
+def test_theta():
+
+    # Test when detuning goes to zero
+
+    theta1 = jc.theta(1, 1, 3)
+
+    assert np.allclose(theta1, np.pi / 4)
+
+def test_check_degeneracy():
+    A = np.array([2, 3, 4, 5, 2])
+    B = np.array([2, 3, 1, 5, 6])
+
+    assert jc.check_degeneracy(A) == True
+    assert jc.check_degeneracy(B) == False
+
+
+def test_E_index():
+
+    # Test for bare
+    H_parameters_bare = (1.0, 2.0, 1.5, 0.1, 3.2)
+    Elist_bare = np.array([0, 1.5, 1, 2.5, 3, 4.5, 7.2, 8.7])
+    expected_indices = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+    result = jc.E_index(H_parameters_bare, Elist_bare, states='bare')
+    assert np.allclose(result, expected_indices)
