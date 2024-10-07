dc605672df6b4a5732b0be7600cf72730b4a5623
“abejarano002” <abejaranosanchez@gmail.com>
2024-10-07 17:16:10 +0200

    black

--- nanocavity/tests/test_master_equation.py
+++ nanocavity/tests/test_master_equation.py
@@ -171,14 +171,14 @@ def test_g2():
     L = no.liouvillian(H, ap + am)
     J = no.jump(am)
     tlist = np.linspace(0, 200, 2)
-    for method in ['eigen',  'direct']:
+    for method in ["eigen", "direct"]:
         g2 = nme.g2(L, J, tlist, method)
         assert np.isclose(g2[0], 2)  # g2(0) = 2 in thermal equilibrium
         assert np.isclose(g2[-1], 1)  # g2(infty) = 1, uncorrelated
-    
+
     tlist = np.arange(200)
     g2 = nme.g2(L, J, tlist)
-    
+
     assert np.all(g2[:-1] > g2[1:])  # decreasing function of delay
     g2b, Mk, E = nme.g2(L, J, tlist, ret_data=True)
     assert np.allclose(g2, g2b)
