8562620c15a49c1337da19da8b47935ed90b832c
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2024-10-09 13:55:44 +0200

    added check that qutip and nanocavity describe the same coherent evolution with L

---
+++ nanocavity/qutip/tests/test_operators.py
@@ -0,0 +1,25 @@
+import numpy as np
+import nanocavity.operators as no
+import nanocavity.tls as ntls
+import nanocavity.qutip.tls as nqtls
+import qutip as qt
+import pytest
+
+
+def test_liouvillian_basic():
+    Eg, Delta, hw_ph, g_ph, U = 0.1, 0.9, 1.1, 0.05, 1.2
+    max_bosons = 3
+
+    Hnc0, Hnc1, _ = ntls.Hamiltonian(Eg, Delta, hw_ph, g_ph, U, max_bosons)
+    Hqt0, Hqt1, _ = nqtls.Hamiltonian(Eg, Delta, hw_ph, g_ph, U, max_bosons)
+
+    d = Hnc0.shape[0]
+    d4 = (d, d, d, d)
+
+    # check coherent evolution
+    for c in [0, 1]:
+        L_nc = no.liouvillian(Hnc0 + c * Hnc1).reshape(d4)
+        # note that qutip uses F-order memory layout
+        L_qt = qt.liouvillian(Hqt0 + c * Hqt1, []).full()
+        assert np.allclose(L_nc, L_qt.reshape(d4, order="F"))
+        assert not np.allclose(L_nc, L_qt.reshape(d4, order="C"))
