bd44b1a8156846c76027660ba2a7a461ea3537f6
Moritz Frankerl <moritz.frankerl@proton.me>
2025-03-11 09:52:47 +0100

    Implemented Quentin's version of FC matrix elements

--- nanocavity/franck_condon.py
+++ nanocavity/franck_condon.py
@@ -1,32 +1,30 @@
 import numpy as np
+import scipy.special as sp
 from math import factorial
 
 
-def FC_factor(n, m, g_ph):
-    """Function to calculate the Franck-Condon Factors
+def FC_factor(nr_bosons, g):
+    """Function to calculate the squared Franck-Condon Factors
     Parameters:
     --------------
-        n: int
-        bosonic quantum number
-        m: int
-        bosonic quantum number
-        g_ph: float
-        monopolar coupling
+        nr_bosons: int
+        number of considered bosons in the system
+        g: float
+        coupling strength
     Returns:
     --------------
-        FC: float
-        Franck-Condon Factor F_{nm}(g_ph)
+        FC: np.array
+        Matrix containing the Franck-Condon Factors
+        Probably calculated according to:
+            Koch, von Oppen, Andreev, PRB 74, 205438 (2006),
+            Theory of the Franck-Condon blockade regime
     """
-    if n > m:
-        n, m = m, n
-    FC_summand = (
-        lambda k: (-(g_ph**2)) ** k
-        * np.sqrt(factorial(n) * factorial(m))
-        * g_ph ** (np.abs(n - m))
-        * np.exp(-(g_ph**2) / 2)
-        / factorial(k)
-        / factorial(n - k)
-        / factorial(k + np.abs(m - n))
-    )
-    FC = np.abs(sum(map(FC_summand, np.arange(n + 1)))) ** 2
-    return FC
+    a = np.arange(nr_bosons + 1)
+    nr_bosons, m = np.meshgrid(a, a)
+    q = np.minimum(nr_bosons, m)
+    Q = np.maximum(nr_bosons, m)
+    fq = sp.factorial(q)
+    fQ = sp.factorial(Q)
+    L = sp.eval_genlaguerre(q, Q - q, g**2)
+    M2 = fq / fQ * np.exp(-(g**2)) * (g ** (Q - q) * L) ** 2
+    return M2
