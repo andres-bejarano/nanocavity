6897dcb25adbc5e3cd71c75eec30919f42ba3ebc
Moritz Frankerl <moritz.frankerl@proton.me>
2025-03-11 09:52:53 +0100

    updated tests

--- nanocavity/tests/test_franck_condon.py
+++ nanocavity/tests/test_franck_condon.py
@@ -1,12 +1,13 @@
 import pytest
 import nanocavity.franck_condon as nfc
 import numpy as np
+import math
 
 
 # The following functions are taken from: Mitra, Aleiner and Millis, PRB 69, 245302 (2004)
 # Title: Phonon effects in molecular transistors: Quantal and classical treatment
 def FC_0n(n, g):
-    return np.exp(-(g**2) / 2) * g**n / np.sqrt(np.math.factorial(n))
+    return np.exp(-(g**2) / 2) * g**n / np.sqrt(math.factorial(n))
 
 
 def FC_11(g):
@@ -24,8 +25,10 @@ def FC_22(g):
 def test_franck_condon():
     g_vec = [0.1, 0.5, 1, 1.5, 2]
     for g in g_vec:
-        assert np.allclose(FC_0n(0, g) ** 2, nfc.FC_factor(0, 0, g))
-        assert np.allclose(FC_0n(1, g) ** 2, nfc.FC_factor(0, 1, g))
-        assert np.allclose(FC_11(g) ** 2, nfc.FC_factor(1, 1, g))
-        assert np.allclose(FC_21(g) ** 2, nfc.FC_factor(2, 1, g))
-        assert np.allclose(FC_22(g) ** 2, nfc.FC_factor(2, 2, g))
+        FC_mat = nfc.FC_factor(5, g)
+        assert np.allclose(FC_0n(0, g) ** 2, FC_mat[0, 0])
+        assert np.allclose(FC_0n(1, g) ** 2, FC_mat[0, 1])
+        assert np.allclose(FC_11(g) ** 2, FC_mat[1, 1])
+        assert np.allclose(FC_21(g) ** 2, FC_mat[2, 1])
+        assert np.allclose(FC_22(g) ** 2, FC_mat[2, 2])
+        assert np.allclose(FC_mat, FC_mat.T)
