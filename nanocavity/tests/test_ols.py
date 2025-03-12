48715c8a54cab430bf451934bbf6acfe9e996af7
Moritz Frankerl <moritz.frankerl@proton.me>
2025-03-12 09:10:24 +0100

    clean up test script

--- nanocavity/tests/test_ols.py
+++ nanocavity/tests/test_ols.py
@@ -5,13 +5,13 @@ from secondquant.operator import Operator
 
 def test_hamiltonian():
     Hs, anni_ops, num_ops = ols.Hamiltonian(1)
-    assert isinstance(Hs, sq.operator.operator.Operator)
+    assert isinstance(Hs, Operator)
     assert isinstance(anni_ops, list)
-    assert isinstance(anni_ops[0], sq.operator.operator.Operator)
-    assert isinstance(anni_ops[1], sq.operator.operator.Operator)
+    assert isinstance(anni_ops[0], Operator)
+    assert isinstance(anni_ops[1], Operator)
     assert isinstance(num_ops, list)
-    assert isinstance(num_ops[0], sq.operator.operator.Operator)
-    assert isinstance(num_ops[1], sq.operator.operator.Operator)
+    assert isinstance(num_ops[0], Operator)
+    assert isinstance(num_ops[1], Operator)
 
 
 def test_lang_firsov_transform():
@@ -34,5 +34,5 @@ def test_collapse_electronic():
     assert isinstance(coll_list, list)
     assert isinstance(coll_list[0], list)
     assert len(coll_list) == 4
-    coll_tot = ols.collapse_electronic(Dg, basis, 1, -1, 1e-4, 5e-4, 1e-3, total=True)
-    assert len(coll_tot) != 4
+    coll_tot = ols.collapse_electronic(D, basis, 1, -1, 1e-4, 5e-4, 1e-3, total=True)
+    assert len(coll_tot) == len(list(chain.from_iterable(coll_list)))
