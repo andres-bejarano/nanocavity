e9a28671f05e6e863052e6c4e7157ec874cb8d1d
“abejarano002” <abejaranosanchez@gmail.com>
2025-03-17 17:35:08 +0100

    fixing test

--- nanocavity/tests/test_ols.py
+++ nanocavity/tests/test_ols.py
@@ -30,9 +30,10 @@ def test_collapse_electronic():
     D, A = ols.Lang_Firsov_transform(D, a_ph, g_ph)
     basis = Hs.eigh()
 
-    coll_list = ols.collapse_electronic(D, basis, 1, -1, 1e-4, 5e-4, 1e-3)
-    assert isinstance(coll_list, list)
-    assert isinstance(coll_list[0], list)
+    c_pL, c_mL, c_pR, c_mR = ols.collapse_electronic(D, basis, 1, -1, 1e-4, 5e-4, 1e-3)
+    coll_list = [c_pL, c_mL, c_pR, c_mR]
+    for c in coll_list:
+        assert isinstance(c, list)
     assert len(coll_list) == 4
     coll_tot = ols.collapse_electronic(D, basis, 1, -1, 1e-4, 5e-4, 1e-3, total=True)
     assert len(coll_tot) == len(list(chain.from_iterable(coll_list)))
