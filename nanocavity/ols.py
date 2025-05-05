d2aadf48eb3e273e6bd9952ead56cb65ba4d4dcb
Moritz Frankerl <moritz.frankerl@proton.me>
2025-05-05 10:16:54 +0200

    removed ols dissipator

--- nanocavity/ols.py
+++ nanocavity/ols.py
@@ -95,31 +95,6 @@ def collapse_electronic(D, basis, VL, VR, Gamma_L, Gamma_R, kT, total=False, cut
     return c_pL, c_mL, c_pR, c_mR
 
 
-def dissipator(c_ops, method="kron"):
-    if not isinstance(c_ops, list):
-        raise TypeError("c_ops must be a list")
-    dim = c_ops[0].shape[0]
-    Id = np.eye(dim)
-    # Look https://arxiv.org/pdf/1504.05266
-    # Attention: The paper uses column stacking
-    # This function uses row stacking
-    D = 0
-    for c1 in c_ops:
-        for c2 in c_ops:
-            cdc = c1.conj().T @ c2
-            if method == "einsum":
-                D += np.einsum("ik,jl->ijkl", c1, c2.conj())
-                D -= 0.5 * np.einsum("ik,jl->ijkl", Id, cdc)
-                D -= 0.5 * np.einsum("ik,jl->ijkl", cdc.conj(), Id)
-            elif method == "kron":
-                D += np.kron(c1, c2.conj())
-                D -= 0.5 * np.kron(Id, cdc)
-                D -= 0.5 * np.kron(cdc.conj(), Id)
-    if method == "einsum":
-        D = D.reshape((dim**2, dim**2))
-    return D
-
-
 def liouvillian(
     dg, a_ph, Hs, g_ph, VL, VR, Gamma_L, Gamma_R, kappa, kT, cond=False, method="kron"
 ):
