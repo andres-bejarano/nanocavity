6a2bda62a9d5f475cf43cc2b8241a4f0fc860bae
Moritz Frankerl <moritz.frankerl@proton.me>
2025-05-07 16:29:59 +0200

    removed the squared function. Added the phase to the implementation of FC and updated documentation

--- nanocavity/franck_condon.py
+++ nanocavity/franck_condon.py
@@ -6,10 +6,15 @@ def FC(a, b, g):
     """
     Franck-Condon matrix element
 
-    See Eq. (8) in:
-    J. Koch, F. von Oppen, and A. V. Andreev
-    Theory of the Franck-Condon blockade regime
-    Phys. Rev. B 74, 205438 (2006)
+    See Eq. A(2) in:
+    A. Yar, A. Donarini, S. Koller and M. Grifoni
+    Dynamical symmetry breaking in vibration-assisted transport through nanostructures
+    Phys. Rev. B 84, 115432 (2011)
+
+    See Eq. B(3) in:
+    J. Koch, F. von Oppen, Y. Oreg and E. Sela
+    Thermopower of single-molecule devices
+    Phys. Rev. B 70, 195107 (2004)
 
     Parameters
     ----------
@@ -32,38 +37,14 @@ def FC(a, b, g):
     fq = sp.factorial(q)
     fQ = sp.factorial(Q)
     L = sp.eval_genlaguerre(q, Q - q, g**2)
-    M = np.sqrt(fq / fQ) * np.exp(-(g**2) / 2) * g ** (Q - q) * L
+
+    H = np.ones(n.shape)
+    exp_arr = m - n
+    H[exp_arr > 0] = (-1) ** exp_arr[exp_arr > 0]
+    M = H * np.sqrt(fq / fQ) * np.exp(-(g**2) / 2) * g ** (Q - q) * L
 
     if np.isscalar(a) and np.isscalar(b):
         return M[0].item()
     elif np.isscalar(a) or np.isscalar(b):
         return M.ravel()
     return M
-
-
-def FC2(a, b, g):
-    """Squared Franck-Condon matrix elements M^2_{a,b}
-
-    See Eq. (8) in:
-    J. Koch, F. von Oppen, and A. V. Andreev
-    Theory of the Franck-Condon blockade regime
-    Phys. Rev. B 74, 205438 (2006)
-
-    Parameters
-    ----------
-    a : int or array-like
-       first index
-    b : int or array-like
-       second index
-    g : float
-       coupling strength
-
-    Returns
-    -------
-    M2 : np.array or float
-       Squared Franck-Condon matrix element(s)
-    """
-    M = FC(a, b, g)
-    M2 = M * M
-
-    return M2
