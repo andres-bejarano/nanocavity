0f538a4ca9df431dbe85271bbd0f58bd863d5b5d
Moritz Frankerl <moritz.frankerl@proton.me>
2025-05-20 12:52:27 +0200

    update tls

--- nanocavity/tls.py
+++ nanocavity/tls.py
@@ -161,18 +161,18 @@ def collapses(H, ops, VL, VR, kappa, Gamma_L, Gamma_R, kT, hw_ph, cutoff=0):
 
     # left electrode
     c_gpL, c_gmL = no.collapses(
-        dg, basis, kT, "fermionic", Gamma_L, mu=VL, cutoff=cutoff
+        dg, basis, kT, "fermionic", Gamma_L, 1e-6, mu=VL, cutoff=cutoff
     )
     c_epL, c_emL = no.collapses(
-        de, basis, kT, "fermionic", Gamma_L, mu=VL, cutoff=cutoff
+        de, basis, kT, "fermionic", Gamma_L, 1e-6, mu=VL, cutoff=cutoff
     )
 
     # right electrode
     c_gpR, c_gmR = no.collapses(
-        dg, basis, kT, "fermionic", Gamma_R, mu=VR, cutoff=cutoff
+        dg, basis, kT, "fermionic", Gamma_R, 1e-6, mu=VR, cutoff=cutoff
     )
     c_epR, c_emR = no.collapses(
-        de, basis, kT, "fermionic", Gamma_R, mu=VR, cutoff=cutoff
+        de, basis, kT, "fermionic", Gamma_R, 1e-6, mu=VR, cutoff=cutoff
     )
 
     # cavity mode
@@ -180,8 +180,8 @@ def collapses(H, ops, VL, VR, kappa, Gamma_L, Gamma_R, kT, hw_ph, cutoff=0):
     # we need to use the full cavity dissipator (see eq 6.37 in D.F.Walls and Gererd J. Milburn -  Quantum Optics).
     nb_p = ndist.bose_einstein(hw_ph, kT)
     nb_m = 1 + nb_p
-    c_ap = [np.sqrt(kappa * nb_p) * a.d.toarray()]
-    c_am = [np.sqrt(kappa * nb_m) * a.toarray()]
+    c_ap = {"f": [np.sqrt(kappa * nb_p) * a.d.toarray()]}
+    c_am = {"f": [np.sqrt(kappa * nb_m) * a.toarray()]}
 
     Plus = [c_gpL, c_epL, c_gpR, c_epR, c_ap]
     Minus = [c_gmL, c_emL, c_gmR, c_emR, c_am]
