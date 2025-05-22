442f8379e678221c0f61dadf19eea1fcf1385ce8
“abejarano002” <abejaranosanchez@gmail.com>
2025-05-22 06:12:30 +0200

    index functions

--- nanocavity/tests/test_ols.py
+++ nanocavity/tests/test_ols.py
@@ -56,3 +56,57 @@ def test_liouvillian():
     assert np.allclose(L.shape[0], Hs.toarray().shape[0] ** 2)
     assert np.allclose(L.shape[1], Hs.toarray().shape[1] ** 2)
     assert np.allclose(L[0, 7], kappa)
+
+def test_get_diagonal_indices():
+    Nmax = 5 
+    index = nols.get_diagonal_indices(max_bosons=Nmax, cutoff=2)
+
+    assert len(index) == 6
+    for i in range(3):
+        assert index[i] == [i, i]
+        assert index[i + 3] == [Nmax + 1 + i, Nmax + 1 + i]
+
+def get_basis_index(n, q, max_bosons):
+
+    max_bosons = 4
+    for n in range(max_bosons + 1):
+        result = get_basis_index(n, 0, max_bosons)
+        expected = n
+        assert result == expected
+
+    # Test for q = 1
+    for n in range(max_bosons + 1):
+        result = get_basis_index(n, 1, max_bosons)
+        expected = max_bosons + 1 + n
+        assert result == expected
+
+    # Test for invalid charge
+    try:
+        get_basis_index(0, 2, max_bosons)
+        assert False, "Expected ValueError for invalid charge q=2"
+    except ValueError:
+        pass  # This is the expected behavior
+
+def test_get_vectorized_rho_index():
+    # we check population index
+    N_ph_max = 4
+    index_h, index_e = [], []
+    for i in range(3):
+        index_h.append(nols.get_vectorized_rho_index(n1=i, q1=0, n2=i, q2=0, max_bosons=N_ph_max))
+        index_e.append(nols.get_vectorized_rho_index(n1=i, q1=1, n2=i, q2=1, max_bosons=N_ph_max))
+    index = index_h +  index_e
+
+    # index_by_hand
+    dim = 2 * (N_ph_max + 1)
+
+    i_h0 = 0
+    i_h1 = dim + 1
+    i_h2 = i_h1 + dim + 1
+
+    i_e0 = (N_ph_max + 1) *  dim + (N_ph_max + 1)
+    i_e1 = i_e0 + dim + 1
+    i_e2 = i_e1 + dim + 1
+
+    index2 = [i_h0, i_h1, i_h2, i_e0, i_e1, i_e2]
+
+    assert index == index2 
