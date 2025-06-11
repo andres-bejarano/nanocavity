0198e476fa98c17f0c3346f5c40319960e0a4747
“abejarano002” <abejaranosanchez@gmail.com>
2025-06-11 08:07:48 +0200

    test

--- nanocavity/tests/test_ols.py
+++ nanocavity/tests/test_ols.py
@@ -7,8 +7,6 @@ from itertools import chain
 from secondquant.operator import Operator
 import numpy as np
 import secondquant as sq
-import nanocavity.franck_condon as nfc
-import nanocavity.master_equation as nme
 
 
 def test_hamiltonian():
@@ -48,13 +46,13 @@ def test_liouvillian():
     hw_ph = 1
     Hs, [dg, a_ph], [ng, n_ph] = nols.Hamiltonian(hw_ph, max_bosons=2)
 
-    Gamma_s = 5e-4
-    Gamma_t = 1e-4
+    Gamma_s = 1e-4
+    Gamma_t = 5e-4
     kappa = 0.1
     kT = 1e-3
     g_ph = 0.5
-    Vs = 1.5
-    Vt = -0.5
+    Vs = -0.5
+    Vt = 1.5
 
     L = nols.liouvillian(dg, a_ph, Hs, g_ph, Vs, Vt, Gamma_s, Gamma_t, kappa, kT)
 
@@ -64,338 +62,29 @@ def test_liouvillian():
 
 
 
-def P0_Q0_st(VL, VR, GL, GR, g_ph, hw_ph):
-    kT = 0
-    Gp00 = G_p_nm_a(0, 0, g_ph, VL, GL, kT) + G_p_nm_a(0, 0, g_ph, VR, GR, kT)
-    Gp10 = G_p_nm_a(1, 0, g_ph, VL, GL, kT) + G_p_nm_a(1, 0, g_ph, VR, GR, kT)
-    Gm00 = G_m_nm_a(0, 0, g_ph, VL, GL, kT) + G_m_nm_a(0, 0, g_ph, VR, GR, kT)
-    Gm10 = G_m_nm_a(1, 0, g_ph, VL, GL, kT) + G_m_nm_a(1, 0, g_ph, VR, GR, kT)
-    P0 = Gm00 + Gm10
-    P0 /= Gp00 + Gp10 + Gm00 + Gm10
-    Q0 = Gp00 + Gp10
-    Q0 /= Gp00 + Gp10 + Gm00 + Gm10
-    return P0, Q0
-
-
-def P1_Q1_st(VL, VR, GL, GR, g_ph, hw_ph, kappa):
-    kT = 0
-    Gp10 = G_p_nm_a(1, 0, g_ph, VL, GL, kT) + G_p_nm_a(1, 0, g_ph, VR, GR, kT)
-    Gp11 = G_p_nm_a(1, 1, g_ph, VL, GL, kT) + G_p_nm_a(1, 1, g_ph, VR, GR, kT)
-    Gp21 = G_p_nm_a(2, 1, g_ph, VL, GL, kT) + G_p_nm_a(2, 1, g_ph, VR, GR, kT)
-    Gm10 = G_m_nm_a(1, 0, g_ph, VL, GL, kT) + G_m_nm_a(1, 0, g_ph, VR, GR, kT)
-    Gm11 = G_m_nm_a(1, 1, g_ph, VL, GL, kT) + G_m_nm_a(1, 1, g_ph, VR, GR, kT)
-    Gm21 = G_m_nm_a(2, 1, g_ph, VL, GL, kT) + G_m_nm_a(2, 1, g_ph, VR, GR, kT)
-    P0, Q0 = P0_Q0_st(VL, VR, GL, VR, g_ph, hw_ph)
-
-    P1 = Gm10 / kappa * Q0 + (Gm11 + Gm21) * Gp10 * P0 / kappa**2
-    Q1 = Gp10 / kappa * P0 + (Gp11 + Gp21) * Gm10 * Q0 / kappa**2
-    return P1, Q1
-
-
-def P2_Q2_st(VL, VR, GL, GR, g_ph, hw_ph, kappa):
-    kT = 0
-    P0, Q0 = P0_Q0_st(VL, VR, GL, VR, g_ph, hw_ph)
-    Gp00 = G_p_nm_a(0, 0, g_ph, VL, GL, kT) + G_p_nm_a(0, 0, g_ph, VR, GR, kT)
-    Gp10 = G_p_nm_a(1, 0, g_ph, VL, GL, kT) + G_p_nm_a(1, 0, g_ph, VR, GR, kT)
-    Gp11 = G_p_nm_a(1, 1, g_ph, VL, GL, kT) + G_p_nm_a(1, 1, g_ph, VR, GR, kT)
-    Gp21 = G_p_nm_a(2, 1, g_ph, VL, GL, kT) + G_p_nm_a(2, 1, g_ph, VR, GR, kT)
-    Gp22 = G_p_nm_a(2, 2, g_ph, VL, GL, kT) + G_p_nm_a(2, 2, g_ph, VR, GR, kT)
-    Gm00 = G_m_nm_a(0, 0, g_ph, VL, GL, kT) + G_m_nm_a(0, 0, g_ph, VR, GR, kT)
-    Gm10 = G_m_nm_a(1, 0, g_ph, VL, GL, kT) + G_m_nm_a(1, 0, g_ph, VR, GR, kT)
-    Gm11 = G_m_nm_a(1, 1, g_ph, VL, GL, kT) + G_m_nm_a(1, 1, g_ph, VR, GR, kT)
-    Gm21 = G_m_nm_a(2, 1, g_ph, VL, GL, kT) + G_m_nm_a(2, 1, g_ph, VR, GR, kT)
-    Gm22 = G_m_nm_a(2, 2, g_ph, VL, GL, kT) + G_m_nm_a(2, 2, g_ph, VR, GR, kT)
-
-    P2_f = Gm21 * Gp10 * P0 / (2 * kappa**2)
-    P2_s = ((Gm21 * (Gp11 + Gp21) * Gm10 + 0.5 * Gm22 * Gp21 * Gm10) * Q0) / (
-        2 * kappa**3
-    )
-    P2 = P2_f + P2_s
-
-    Q2_f = Gp21 * Gm10 * Q0 / (2 * kappa**2)
-    Q2_s = ((Gp21 * (Gm11 + Gm21) * Gp10 + 0.5 * Gp22 * Gm21 * Gp10) * P0) / (
-        2 * kappa**3
-    )
-    Q2 = Q2_f + Q2_s
-    return P2, Q2
-
-
-def G_p_nm_a(n, m, g_ph, V, Gamma, kT):
-    Fnm = nfc.FC(n, m, g_ph) ** 2
-    f = nd.fermi_dirac(n - m, kT, V)
-    return Gamma * f * Fnm
-
-
-def G_m_nm_a(n, m, g_ph, V, Gamma, kT):
-    Fnm = nfc.FC(n, m, g_ph) ** 2
-    f = nd.fermi_dirac(m - n, kT, V)
-    return Gamma * (1 - f) * Fnm
-
-
 def test_populations():
     hw_ph = 1
-    nmax = 6
-    Hs, [dg, a_ph], [ng, n_ph] = nols.Hamiltonian(hw_ph, max_bosons=nmax)
+    max_bosons = 5
+    Hs, [dg, a_ph], [ng, n_ph] = nols.Hamiltonian(hw_ph, max_bosons=max_bosons)
 
-    kappa = 0.1
-    kT = 0.001
-
-    VL = 1.1
-    VR = -0.1
-    GL = 5e-4
-    GR = 1e-4
-    Gamma = GL + GR
-
-    g_ph = 0.4
-    L = nols.liouvillian(dg, a_ph, Hs, g_ph, VL, VR, GL, GR, kappa, kT)
-    rho_st = np.real(nme.stationary(L))
-    P0 = nme.reduced_population([ng, n_ph], rho_st, [0, 0])
-    P1 = nme.reduced_population([ng, n_ph], rho_st, [0, 1])
-    P2 = nme.reduced_population([ng, n_ph], rho_st, [0, 2])
-    Q0 = nme.reduced_population([ng, n_ph], rho_st, [1, 0])
-    Q1 = nme.reduced_population([ng, n_ph], rho_st, [1, 1])
-    Q2 = nme.reduced_population([ng, n_ph], rho_st, [1, 2])
-    P0_an, Q0_an = P0_Q0_st(VL, VR, GL, GR, g_ph, hw_ph)
-    P1_an, Q1_an = P1_Q1_st(VL, VR, GL, GR, g_ph, hw_ph, kappa)
-    P2_an, Q2_an = P2_Q2_st(VL, VR, GL, GR, g_ph, hw_ph, kappa)
-
-    assert np.isclose(P0_an, P0, atol=(Gamma / kappa))
-    assert np.isclose(Q0_an, Q0, atol=(Gamma / kappa))
-    assert np.isclose(P1_an, P1, atol=(Gamma / kappa) ** 2)
-    assert np.isclose(Q1_an, Q1, atol=100 * (Gamma / kappa) ** 2)
-    assert np.isclose(P2_an, P2, atol=(Gamma / kappa) ** 3)
-    assert np.isclose(Q2_an, Q2, atol=(Gamma / kappa) ** 3)
-
-def test_G_pm_nm_a():
-    qi, qf = np.meshgrid([0, 1], [0, 1], indexing="ij")
-    qf, qi = np.meshgrid(1, 3, indexing="ij")
-    kT = 1e-3
-    g_ph = 0.3
-    Gp, Gm = nols.G_pm_nm_a(1, 1, 1, 0.5, kT)
-    assert np.isclose(Gp, 0)
-    assert np.isclose(Gm, 0)
-    Gp, Gm = nols.G_pm_nm_a(3, 1, g_ph, 0.5, kT)
-    assert np.isclose(Gp, 0)
-    assert np.isclose(Gm, 0)
-
-    Gp, Gm = nols.G_pm_nm_a(3, 1, g_ph, 2.5, kT)
-    assert np.isclose(Gp, nfc.FC(3, 1, g_ph) ** 2)
-    assert np.isclose(Gm, 0)
-
-    # Add these two tests back for Gp and Gm
-    Gp, Gm = nols.G_pm_nm_a(1, 3, g_ph, 0.5, kT)
-    assert np.isclose(Gp, nfc.FC(1, 3, g_ph) ** 2)
-    assert np.isclose(Gm, nfc.FC(1, 3, g_ph) ** 2)
-
-    Gp, Gm = nols.G_pm_nm_a(1, 3, g_ph, 2.5, kT)
-    assert np.isclose(Gp, nfc.FC(1, 3, g_ph) ** 2)
-    assert np.isclose(Gm, 0)
-
-    Gp, Gm = nols.G_pm_nm_a(1, 3, g_ph, 2.5, kT, DE=0.3)
-    assert np.isclose(Gp, nfc.FC(1, 3, g_ph) ** 2)
-    assert np.isclose(Gm, 0)
-
-    Gp, Gm = nols.G_pm_nm_a(1, 3, g_ph, 2.5, kT, DE=0.8)
-    assert np.isclose(Gp, nfc.FC(1, 3, g_ph) ** 2)
-    assert np.isclose(Gm, nfc.FC(1, 3, g_ph) ** 2)
-
-    Gp, Gm = nols.G_pm_nm_a([0, 1], [0, 1], g_ph, 0.5, kT)
-    assert Gp.shape == (2, 2)
-    assert Gm.shape == (2, 2)
-    assert np.isclose(Gp[0, 0], nfc.FC(0, 0, g_ph) ** 2)
-    assert np.isclose(Gp[0, 1], nfc.FC(1, 0, g_ph) ** 2)
-    assert np.isclose(Gp[1, 0], 0)
-    assert np.isclose(Gp[1, 1], nfc.FC(1, 1, g_ph) ** 2)
-
-    assert np.isclose(Gm[0, 0], 0)
-    assert np.isclose(Gm[0, 1], nfc.FC(1, 0, g_ph) ** 2)
-    assert np.isclose(Gm[1, 0], 0)
-    assert np.isclose(Gm[1, 1], 0)
-
-    Gp, Gm = nols.G_pm_nm_a([0, 1], 0, g_ph, 0.5, kT)
-    assert Gp.shape == (2,)
-    assert Gm.shape == (2,)
-    
-def test_get_diagonal_indices():
-    Nmax = 5
-    index = nols.get_diagonal_indices(max_bosons=Nmax, cutoff=2)
-
-    assert len(index) == 6
-    for i in range(3):
-        assert index[i] == [i, i]
-        assert index[i + 3] == [Nmax + 1 + i, Nmax + 1 + i]
-
-
-def get_basis_index(n, q, max_bosons):
-
-    max_bosons = 4
-    for n in range(max_bosons + 1):
-        result = get_basis_index(n, 0, max_bosons)
-        expected = n
-        assert result == expected
-
-    # Test for q = 1
-    for n in range(max_bosons + 1):
-        result = get_basis_index(n, 1, max_bosons)
-        expected = max_bosons + 1 + n
-        assert result == expected
-
-    # Test for invalid charge
-    try:
-        get_basis_index(0, 2, max_bosons)
-        assert False, "Expected ValueError for invalid charge q=2"
-    except ValueError:
-        pass  # This is the expected behavior
-
-
-def test_get_vectorized_rho_index():
-    # we check population index
-    N_ph_max = 4
-    index_h, index_e = [], []
-    for i in range(3):
-        index_h.append(
-            nols.get_vectorized_rho_index(n1=i, q1=0, n2=i, q2=0, max_bosons=N_ph_max)
-        )
-        index_e.append(
-            nols.get_vectorized_rho_index(n1=i, q1=1, n2=i, q2=1, max_bosons=N_ph_max)
-        )
-    index = index_h + index_e
-
-    # index_by_hand
-    dim = 2 * (N_ph_max + 1)
-
-    i_h0 = 0
-    i_h1 = dim + 1
-    i_h2 = i_h1 + dim + 1
-
-    i_e0 = (N_ph_max + 1) * dim + (N_ph_max + 1)
-    i_e1 = i_e0 + dim + 1
-    i_e2 = i_e1 + dim + 1
-
-    index2 = [i_h0, i_h1, i_h2, i_e0, i_e1, i_e2]
-
-    assert index == index2
-
-
-def test_Liovillian_matrix_elements():
-    hw_ph = 1
-    g_ph = 0.5
-    N_ph_max = 5
-
-    # we need Gamma<<kappa
-    Gamma_s, Gamma_t = 5e-6, 1e-6
     kappa = 0.1
     kT = 0.01
 
-    # first photon emission threshold
-    Vs, Vt = 0.5, -1.5
-    Hs, [dg, a_ph], [ng, n_ph] = nols.Hamiltonian(hw_ph=hw_ph, max_bosons=N_ph_max)
-    L = nols.liouvillian(dg, a_ph, Hs, g_ph, Vs, Vt, Gamma_s, Gamma_t, kappa, kT)
 
-    # population block numerically
-    index_h, index_e = [], []
-    for i in range(3):
-        index_h.append(
-            nols.get_vectorized_rho_index(n1=i, q1=0, n2=i, q2=0, max_bosons=N_ph_max)
-        )
-        index_e.append(
-            nols.get_vectorized_rho_index(n1=i, q1=1, n2=i, q2=1, max_bosons=N_ph_max)
-        )
-    index = index_h + index_e
 
-    Lpop_num = L[np.ix_(index, index)]
+    Vt = 0.5
+    Vs = -1.5
+    Gamma_t = 1e-6
+    Gamma_s = 5e-6
 
-    # population block anallytically
-    # Franck-Condon
-    n = np.arange(3)
-    F = nfc.FC(n, n, g_ph)
-    F2 = F**2
+    g_ph = 0.5
 
-    # according to eq S155
-    GammaS = Gamma_s * F2
-    GammaT = Gamma_t * F2
+    for VL, VR in [[1.5, 0.5], [2.5, -0.5], [1.5, -1.5]]:
+        L = nols.liouvillian(dg, a_ph, Hs, g_ph, Vs, Vt, Gamma_s, Gamma_t, kappa, kT)
+        rho_st = np.real(nme.stationary(L))
+        index = nols.get_diagonal_indices(max_bosons=max_bosons, cutoff=2)
 
-    GammaP = np.array(
-        [
-            [GammaS[0, 0], GammaS[0, 1], GammaT[0, 2] + GammaS[0, 2]],
-            [0, GammaS[1, 1], GammaS[1, 2]],
-            [0, 0, GammaS[2, 2]],
-        ]
-    )
-
-    GammaM = np.array(
-        [
-            [GammaT[0, 0], GammaT[0, 1] + GammaS[0, 1], GammaT[0, 2] + GammaS[0, 2]],
-            [GammaT[1, 0], GammaT[1, 1], GammaT[1, 2] + GammaS[1, 2]],
-            [0, GammaT[2, 1], GammaT[2, 2]],
-        ]
-    )
-
-    # Eq S155
-    Lpop_an = np.array(
-        [
-            [-GammaP[0, 0], kappa, 0, GammaM[0, 0], GammaM[0, 1], GammaM[0, 2]],
-            [0, -kappa, 2 * kappa, GammaM[1, 0], GammaM[1, 1], GammaM[1, 2]],
-            [0, 0, -2 * kappa, 0, GammaM[2, 1], GammaM[2, 2]],
-            [
-                GammaP[0, 0],
-                GammaP[0, 1],
-                GammaP[0, 2],
-                -GammaM[0, 0] - GammaM[1, 0],
-                kappa,
-                0,
-            ],
-            [0, GammaP[1, 1], GammaP[1, 2], 0, -kappa, 2 * kappa],
-            [0, 0, GammaP[2, 2], 0, 0, -2 * kappa],
-        ]
-    )
-
-    # numerical vs anallytical  L population block
-    assert np.allclose(Lpop_num.real, Lpop_an, atol=(Gamma_t / kappa))
-
-    # checking L coherences blocks
-
-    # S74
-    e0_h1 = nols.get_vectorized_rho_index(n1=0, q1=1, n2=1, q2=0, max_bosons=N_ph_max)
-    assert np.allclose(L[e0_h1, e0_h1], 1j * hw_ph - (kappa * g_ph**2 / 2 + kappa) / 2)
-
-    # S75
-    h0_e1 = nols.get_vectorized_rho_index(n1=0, q1=0, n2=1, q2=1, max_bosons=N_ph_max)
-    assert np.allclose(L[h0_e1, h0_e1], 1j * hw_ph - (kappa * g_ph**2 / 2 + kappa) / 2)
-
-    # S76 -77
-    h0_e0 = nols.get_vectorized_rho_index(n1=0, q1=0, n2=0, q2=1, max_bosons=N_ph_max)
-    h1_e1 = nols.get_vectorized_rho_index(n1=1, q1=0, n2=1, q2=1, max_bosons=N_ph_max)
-
-    assert np.allclose(L[h0_e0, h0_e0], -kappa * g_ph**2 / 4, atol=Gamma_t / kappa)
-    assert np.allclose(L[h0_e0, h1_e1], -kappa)  # why - kappa? should be +
-    assert np.allclose(
-        L[h1_e1, h1_e1], -kappa * g_ph**2 / 4 - kappa, atol=Gamma_t / kappa
-    )
-
-    # S78 - 79
-    h0_h1 = nols.get_vectorized_rho_index(n1=0, q1=0, n2=1, q2=0, max_bosons=N_ph_max)
-    e0_e1 = nols.get_vectorized_rho_index(n1=0, q1=1, n2=1, q2=1, max_bosons=N_ph_max)
-
-    assert np.allclose(L[h0_h1, h0_h1], 1j * hw_ph - kappa / 2)
-    assert np.allclose(L[e0_e1, e0_e1], 1j * hw_ph - kappa / 2)
-
-    # virtual transitions
-
-    Gamma_t00_1 = Gamma_t * nfc.FC(0, 0, g_ph) * nfc.FC(1, 1, g_ph)
-    Gamma_s00_1 = Gamma_s * nfc.FC(0, 0, g_ph) * nfc.FC(1, 1, g_ph)
-    assert np.allclose(L[h0_h1, e0_e1], Gamma_t00_1)
-    assert np.allclose(L[e0_e1, h0_h1], Gamma_s00_1)
-
-    # coherences zero?
-    rho_st = nme.stationary(L)
-    coherences = rho_st - np.diag(np.diag(rho_st))
-    max_off = np.max(np.abs(coherences))
-    assert max_off < 1e-15
-
-    # cross terms in correlations are zero
-
-    M, _ = nme.regression_theorem(a_ph.d, L, ng, cutoff=0)
-    assert np.allclose(M, 0)
-    M, _ = nme.regression_theorem(ng, L, a_ph, cutoff=0)
-    assert np.allclose(M, 0)
+        row, colum = zip(*index)
+        Pnum = rho_st[list(row), list(colum)]
+        Pan = nols.Pst(Vt, Vs, Gamma_t, Gamma_s, g_ph, kappa)
+        assert np.allclose(Pnum, Pan)
